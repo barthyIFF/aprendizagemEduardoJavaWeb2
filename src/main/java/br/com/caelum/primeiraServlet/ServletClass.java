fe25e5d2f8c9ea77d3f2a6031bce99bbd0ae8864
Claudinho <barthy@gmail.com>
2023-07-18 16:25:22 -0300

    primeiro

---
+++ src/main/java/br/com/caelum/primeiraServlet/ServletClass.java
@@ -0,0 +1,51 @@
+package br.com.caelum.primeiraServlet;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class ServletClass
+ */
+@WebServlet("/ServletClass")
+public class ServletClass extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public ServletClass() {
+        // TODO Auto-generated constructor stub
+    }
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		PrintWriter out = response.getWriter();
+		
+		
+		out.print("<html>");
+		out.print("<head>");
+		out.print("<body>");
+		out.print("<h1>Hello Servlet doo eddduardo</h1>");
+		out.print("exemplo de texto");
+		out.print("</body>");
+		out.print("</html>");
+		
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		doGet(request, response);
+	}
+
+}
